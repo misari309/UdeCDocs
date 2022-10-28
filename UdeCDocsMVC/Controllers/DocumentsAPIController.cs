74ef69be0922e85e2a4020bfa68a0cdfe2fb77db
misari309 <90524159+misari309@users.noreply.github.com>
2022-10-28 09:36:17 -0500

    Create DocumentsAPIController.cs

---
+++ UdeCDocsMVC/Controllers/DocumentsAPIController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UdeCDocsMVC.Models;
+
+namespace UdeCDocsMVC.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DocumentsAPIController : ControllerBase
+    {
+        private readonly UdeCDocsContext _context;
+
+        public DocumentsAPIController(UdeCDocsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/DocumentsAPI
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Document>>> GetDocuments()
+        {
+            return await _context.Documents.ToListAsync();
+        }
+
+        // GET: api/DocumentsAPI/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Document>> GetDocument(int id)
+        {
+            var document = await _context.Documents.FindAsync(id);
+            
+            if (document == null)
+            {
+                return NotFound();
+            }
+
+            document.Comments = await _context.Comments.Where(c => c.Iddocument == id).ToListAsync();
+            document.Votes = await _context.Votes.Where(c => c.Iddocument == id).ToListAsync();
+            return document;
+        }
+
+        // PUT: api/DocumentsAPI/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutDocument(int id, Document document)
+        {
+            if (id != document.Iddocument)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(document).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DocumentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/DocumentsAPI
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Document>> PostDocument(Document document)
+        {
+            _context.Documents.Add(document);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetDocument", new { id = document.Iddocument }, document);
+        }
+
+        // DELETE: api/DocumentsAPI/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteDocument(int id)
+        {
+            var document = await _context.Documents.FindAsync(id);
+            if (document == null)
+            {
+                return NotFound();
+            }
+
+            _context.Documents.Remove(document);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool DocumentExists(int id)
+        {
+            return _context.Documents.Any(e => e.Iddocument == id);
+        }
+    }
+}
